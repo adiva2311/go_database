e072fd42a49d320e22a0ffa7c8f65efc3f490330
Adiva Nursuandy Ritonga <adiva.ritonga23@gmail.com>
2025-02-14 02:02:49 +0700

    update

--- sql_test.go
+++ sql_test.go
@@ -88,3 +88,117 @@ func TestQuerySqlComplex(*testing.T) {
 		fmt.Println("created_at:" , created_at)
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := DBConnection()
+	defer db.Close()
+
+	username := "admin'; #" // ini user akan tetap berhasil login walau password salah 
+	password := "salah"
+
+	ctx := context.Background()
+	// Ini adalah contoh query yang salah, karna data akan diterima langsung oleh database dari user iseng
+	sql_query := "SELECT username FROM users WHERE username = '"+username+"' AND password = '"+password+"' LIMIT 1"
+	rows, err := db.QueryContext(ctx, sql_query)
+	if err != nil {
+		panic(err)
+	}
+
+	// Untuk ngecek apakah datanya ada atau tidak
+	if rows.Next(){
+		var username string
+		err := rows.Scan(&username)
+		if err != nil{
+			panic(err)
+		}
+		fmt.Println("Success Login")
+	} else {
+		fmt.Println("Failed Login!!")
+	}
+}
+
+func TestSqlInjectionParameter(t *testing.T) {
+	// Solusi dari SQL Injection adalah kita harus menggunakan Query dengan Paramater, sehingga apa yang di input tidak ada yang ngawur
+	db := DBConnection()
+	defer db.Close()
+
+	username := "admin"
+	password := "admin"
+
+	ctx := context.Background()
+	sql_query := "SELECT username FROM users WHERE username = ? AND password = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx, sql_query, username, password) // Urutkan sesuai dengan sql query
+	if err != nil {
+		panic(err)
+	}
+
+	// Untuk ngecek apakah datanya ada atau tidak
+	if rows.Next(){
+		var username string
+		err := rows.Scan(&username)
+		if err != nil{
+			panic(err)
+		}
+		fmt.Println("Success Login")
+	} else {
+		fmt.Println("Failed Login!!")
+	}
+}
+
+// Ini akan berlaku juga untuk query eksekusi Exec
+func TestExecSqlParameter(t *testing.T) {
+	db := DBConnection()
+	defer db.Close()
+
+	//Diibaratkan input dari user
+	username := "firdaus'; #" //Ini akan di masukkan ke dalam database, tidak akan dianggap sebagai sql query
+	password := "firdaus"
+
+	ctx := context.Background()
+	sql_query := "INSERT INTO users (username, password) VALUES (?, ?)"
+	_, err := db.ExecContext(ctx, sql_query, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("New User Inserted Succesfully")
+}
+
+func TestAutoIncrement(t *testing.T) {
+	db := DBConnection()
+	defer db.Close()
+
+	//Diibaratkan input dari user
+	email := "firdaus@gmail.com"
+	comment := "Aku Ganteng Sekali"
+
+	ctx := context.Background()
+	sql_query := "INSERT INTO reviews (email, comment) VALUES (?, ?)"
+	result, err := db.ExecContext(ctx, sql_query, email, comment)
+	if err != nil {
+		panic(err)
+	}
+
+	insertedId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success Insert Comment with ID :", insertedId)
+
+	data := "SELECT email, comment FROM reviews WHERE id = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx, data, insertedId)
+	if err != nil {
+		panic(err)
+	}
+
+	if rows.Next(){
+		var email, comment string
+		err := rows.Scan(&email, &comment)
+		if err != nil{
+			panic(err)
+		}
+		fmt.Println("email:", email)
+		fmt.Println("comment:", comment)
+	}
+}
